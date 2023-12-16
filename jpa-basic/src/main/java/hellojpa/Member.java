6643ebab64a80b7b560504adc3eb46b3fd2fe918
gussla2206 <gussla2206@gmail.com>
2023-12-17 03:01:30 +0900

    session 4 필드와 컬럼 매핑

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -7,16 +7,22 @@ import java.util.Date;
 public class Member {
     @Id
     private Long id;
-    @Column(name = "name") // DB 컬럼명은 name 이야
+    @Column(name = "name", nullable = false) // DB 컬럼명은 name 이야 update가 될 때 변경되는 것을 금지시킨다.
     private String username;
     private Integer age;
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.ORDINAL)
     private RoleType roleType;
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
-    @Lob // 문자타입은 기본적으로 clob으로 된다. 
+    @Lob // 문자타입은 기본적으로 clob으로 된다.
     private String description;
-    //Getter, Setter…
+
+    @Transient // DB가 아닌 메모리상에서만 사용한다.
+    private int temp;
+
+    public Member(){
+
+    }
 }
