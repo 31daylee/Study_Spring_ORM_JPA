63b84fbbe06237081ea86ab0e447d5477096071a
gussla2206 <gussla2206@gmail.com>
2023-12-29 05:03:30 +0900

    session 6 - 다양한 연관관계매핑

--- jpa-basic/src/main/java/jpabook/jpashop/domain/DeliveryStatus.java
+++ jpa-basic/src/main/java/jpabook/jpashop/domain/DeliveryStatus.java
@@ -1,2 +1,12 @@
-package jpabook.jpashop.domain;public class DeliveryStatus {
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class DeliveryStatus {
+
+    @Id @GeneratedValue
+    private Long id;
 }
